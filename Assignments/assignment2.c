b7cc9f2f3fad1528d79361f557a488174e2cc59b
Pranav K <pranavkcse@gmail.com>
2023-06-28 21:14:38 +0530

    Updated

--- Assignments/assignment2.c
+++ Assignments/assignment2.c
@@ -53,6 +53,52 @@ int main(void) {
   } else {
     printf("Grade: A");
   }
+    // 3.Using the ‘switch case’ write a program to accept an input number from the user and
+    // output the day as follows.
+
+    // Input              Output
+    // 1                  Sunday
+    // 2                  Monday
+    // 3                  Tuesday
+    // 4                  Wednesday
+    // 5                  Thursday
+    // 6                  Friday
+    // 7                  Saturday
+    // Any other input    Invalid
+
+  printf("\n\nQuestion 3\n");
+
+  int number;
+
+  printf("Enter the number of day: ");
+  scanf("%d", &number);
+
+  switch (number) {
+  case 1:
+    printf("Sunday");
+    break;
+  case 2:
+    printf("Monday");
+    break;
+  case 3:
+    printf("Tuesday");
+    break;
+  case 4:
+    printf("Wednesday");
+    break;
+  case 5:
+    printf("Thursday");
+    break;
+  case 6:
+    printf("Friday");
+    break;
+  case 7:
+    printf("Saturday");
+    break;
+  default:
+    printf("Invalid input");
+  }
+return 0;
 
   return 0;
 }
