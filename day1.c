464802d5aff4e16b43b5b0756f45827912f93c2b
Pranav K <pranavkcse@gmail.com>
2023-06-28 20:22:50 +0530

    Part 2

--- day1.c
+++ day1.c
@@ -2,3 +2,20 @@
 
 // Part 2 - Variables, Datatypes & I/O Operations
 
+int main(void){
+    int num1,num2,sum,temp;
+    printf("Enter 2 numbers: ");
+    scanf("%d %d",&num1,&num2);
+    sum = num1+num2;
+    printf("Sum = %d",sum);
+
+    // Swapping
+
+    printf("\n\n\n");
+    printf("Swapping num1:%d and num2:%d",num1,num2);
+    temp=num1;
+    num1=num2;
+    num2=temp;
+    printf("Swapped values : \n num1:%d and num2:%d",num1,num2);
+}
+
